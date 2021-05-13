adec39f30e4b856d90c80f576757decb1e70d559
Eduardo Sohne <dudu.hit@gmail.com>
2021-05-13 13:46:40 -0700

    Add files via upload

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,34 +1,96 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
-import wateringImg from '../assets/watering.png'
 
-export function Welcome(){
+import {
+    SafeAreaView,
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+    StyleSheet,
+    Dimensions
+} from 'react-native'
+
+import { Feather } from '@expo/vector-icons'
+
+import wateringImg from '../assets/watering.png'
+import colors from '../styles/colors';
+
+export function Welcome() {
+
     return (
-        <View style={styles.container}>
-            <Text>
-                Gerencie duas plantas de forma fácil
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>
+                Gerencie {'\n'}
+                suas plantas {'\n'}
+                 de forma fácil
             </Text>
-            <Image source={wateringImg} />
-            <Text>
-                Não esqueça mais de regar suas plantas. 
+
+            <Image
+                source={wateringImg}
+                style={styles.image}
+                resizeMode="contain"
+            />     
+
+            <Text style={styles.subtitle}>
+                Não esqueça mais de regar suas plantas.
                 Nós cuidamos de lembrar você sempre que precisar.
             </Text>
 
-            <TouchableOpacity>
-                <Text>
-                     >
-                </Text>
-            </TouchableOpacity>  
+            <TouchableOpacity
+                style={styles.button}
+                activeOpacity={0.7}
+            >
 
-        </View>
+            <Text style={styles.buttonIcon}>
+                    <Feather
+                        name="chevron-right" 
+                        style={styles.buttonIcon}
+                    />
+            </Text>
+
+
+            </TouchableOpacity>
+
+        </SafeAreaView>
 
     )
 
 }
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     container: {
-       flex: 1,
-       alignItems: 'center',
-       justifyContent: 'space-between'
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around'
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 38
+    },
+    subtitle: {
+        fontSize: 18,
+        textAlign: 'center',
+        paddingHorizontal: 20,
+        color: colors.heading,
+
+    },
+    image: {
+        // height: Dimensions.get('window').width * 1
+    },
+    button: {
+        backgroundColor: colors.green,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 10,
+        height: 56,
+        width: 56
+    },
+    buttonIcon: {
+        fontSize: 32,
+        color: colors.white,
+
     }
 });
