65c3d25073c2bb3a37be77dac76f8b859228dd2b
Eduardo Sohne <dudu.hit@gmail.com>
2021-05-21 17:50:54 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import { Welcome } from './src/pages/Welcome';
-import AppLoading from 'expo-app-loading'
+import { Text, View } from 'react-native';
+import { UserIdentification, Welcome } from './src/pages/UserIdentification';
+import AppLoading from 'expo-app-loading';
 
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold
-} from '@expo-google-fonts/jost'
-
+} from '@expo-google-fonts/jost';
 
 export default function App() {
-  
   const [ fontsLoaded ] = useFonts ({
     Jost_400Regular,
-    Jost_600SemiBold  
+    Jost_600SemiBold
   });
 
-  if (fontsLoaded){
-    return <AppLoading />
-  }
+  if (!fontsLoaded)
+  return <AppLoading />
+ 
   return (
-    <Welcome />
+    <UserIdentification />
   )
 }
 
